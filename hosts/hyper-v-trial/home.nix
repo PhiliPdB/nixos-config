819d22fbcbc3b6149e56963fa78e22b7dc8750df
PhiliPdB <phlpdbrn@gmail.com>
2025-02-07 21:29:37 +0100

    Add taskbar panel to plasma config

--- hosts/hyper-v-trial/home.nix
+++ hosts/hyper-v-trial/home.nix
@@ -23,6 +23,16 @@
   office.enable = true;
   proton-apps.enable = true;
 
+  # Set plasma options
+  plasma = {
+    pinnedItems = [
+      "preferred://filemanager"
+      "preferred://browser"
+      "applications:steam.desktop"
+      "applications:code.desktop"
+    ];
+  };
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
--- user/desktop/plasma.nix
+++ user/desktop/plasma.nix
@@ -1,5 +1,41 @@
-{ lib, user, ... }:
+{ config, lib, user, ... }:
 {
+  options = {
+    plasma = {
+      pinnedItems = lib.mkOption {
+        description = "List of programs to pin to the taskbar";
+        type = with lib.types; listOf str;
+
+        default = [
+          "preferred://filemanager"
+          "preferred://browser"
+          "applications:code.desktop"
+        ];
+      };
+
+      systemTray = {
+        itemsShown = lib.mkOption {
+          description = "List of items to show on the systemtray";
+          type = with lib.types; listOf str;
+
+          default = [
+            "org.kde.plasma.volume"
+            "org.kde.plasma.networkmanagement"
+          ];
+        };
+        itemsHidden = lib.mkOption {
+          description = "List of items to hide from the systemtray";
+          type = with lib.types; listOf str;
+
+          default = [
+            "org.kde.plasma.clipboard"
+            "org.kde.plasma.brightness"
+          ];
+        };
+      };
+    };
+  };
+
   config = lib.mkIf (user.desktop == "plasma") {
     programs.plasma = {
       enable = true;
@@ -8,6 +44,45 @@
         lookAndFeel = "org.kde.breezedark.desktop";
         iconTheme = "Flatery-Blue-Dark";
       };
+
+      # Set virtual desktops
+      kwin.virtualDesktops = {
+        number = 5;
+        rows = 1;
+      };
+
+      # Set panel layout
+      panels = [
+        { # Bottom panel (Windows like)
+          location = "bottom";
+          alignment = "center";
+          lengthMode = "fill";
+          floating = true;
+          height = 32;
+
+          widgets = [
+            "org.kde.plasma.kickoff"
+            {
+              iconTasks = {
+                launchers = config.plasma.pinnedItems;
+              };
+            }
+            "org.kde.plasma.marginsseparator"
+            {
+              systemTray = {
+                items.shown = config.plasma.systemTray.itemsShown;
+                items.hidden = config.plasma.systemTray.itemsHidden;
+              };
+            }
+            {
+              digitalClock = {
+                date.enable = false;
+              };
+            }
+            "org.kde.plasma.notifications"
+          ];
+        }
+      ];
     };
   };
 }
