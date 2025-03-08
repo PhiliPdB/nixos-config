e69604cfddd59b885b18730fc87f4aab382d9a9f
PhiliPdB <phlpdbrn@gmail.com>
2025-03-08 13:57:08 +0100

    Add docker autoPrune config

--- system/docker.nix
+++ system/docker.nix
@@ -1,4 +1,13 @@
 { ... }:
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+
+    autoPrune = {
+      enable = true;
+      dates = "weekly";
+      # Filter stuff older than 30 days
+      flags = [ "-af" "--filter 'until=720h'" ];
+    };
+  };
 }
