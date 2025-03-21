9e4268ddb2d231abcf0efc3643c6bd112d1bd808
PhiliPdB <phlpdbrn@gmail.com>
2025-03-21 20:39:19 +0100

    Start firefox downloads in tmp

--- system/applications/firefox.nix
+++ system/applications/firefox.nix
@@ -2,9 +2,11 @@
 {
   programs.firefox = {
     enable = true;
-    languagePacks = [ "en-US" "nl"];
+    languagePacks = [ "en-US" "nl" ];
 
     preferences = {
+      "browser.download.start_downloads_in_tmp_dir" = true;
+
       "widget.use-xdg-desktop-portal.file-picker" = 1;
     };
   };
