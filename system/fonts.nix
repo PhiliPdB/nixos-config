b1b7c612d5cc5274fd1b6c99f82f8f7d023fd118
PhiliPdB <phlpdbrn@gmail.com>
2025-04-09 19:28:50 +0200

    Add some system fonts

--- system/fonts.nix
+++ system/fonts.nix
@@ -4,6 +4,10 @@
     fira-code
     fira-code-symbols
     (nerdfonts.override { fonts = [ "FiraCode" "DejaVuSansMono" ]; })
+    noto-fonts
+    noto-fonts-emoji
+    open-sans
+    roboto
     vistafonts
   ];
 }
