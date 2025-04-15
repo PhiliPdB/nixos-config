f625c78057415b142a203031e1da63ca50fb6a0e
PhiliPdB <phlpdbrn@gmail.com>
2025-04-15 20:27:38 +0200

    General improvements

--- flake.nix
+++ flake.nix
@@ -2,19 +2,22 @@
   description = "Nixos config flake";
 
   inputs = {
+    # Define nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # Home and Plasma Manager
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+
+    # General theming
     stylix.url = "github:danth/stylix/release-24.11";
   };
 
--- user/desktop/default.nix
+++ user/desktop/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./gtk.nix
+
     ./plasma.nix
   ];
 
