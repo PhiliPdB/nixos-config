2c99b8473076231d66dea1ccf1f485b9f9d725fe
PhiliPdB <phlpdbrn@gmail.com>
2025-09-02 20:26:46 +0200

    Base gpg pinentry on desktop settings

--- user/gpg.nix
+++ user/gpg.nix
@@ -1,10 +1,16 @@
-{ pkgs, ... }:
+{ lib, sysCfg, pkgs, ... }:
+let
+  displayManager = if sysCfg.desktop.enable then sysCfg.desktop.manager else null;
+in
 {
   programs.gpg.enable = true;
 
   services.gpg-agent = {
     enable = true;
-    pinentry.package = pkgs.pinentry-qt;
+    pinentry.package = lib.mkDefault (
+      if displayManager == "plasma" then pkgs.pinentry-qt
+      else pkgs.pinentry-tty
+    );
     enableSshSupport = true;
   };
 }
