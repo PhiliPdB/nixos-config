5b9b7812bf19d02e26e4b357a63223867b2f9f95
PhiliPdB <phlpdbrn@gmail.com>
2025-09-03 10:03:16 +0200

    Start with a wsl config

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     # Define nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Define nixos-wsl
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/release-25.05";
 
     # Home and Plasma Manager
     home-manager = {
@@ -92,6 +94,31 @@
                 unstable-overlay
                 ./hosts/workstation-trial/configuration.nix
                 ./system
+              ];
+            };
+        wsl = 
+          let
+            system = "x86_64-linux";
+            
+            meta = {
+              inherit themesPath;
+              systemName = "nixos-wsl";
+            };
+          in
+            nixpkgs.lib.nixosSystem {
+              inherit system;
+              specialArgs = { inherit inputs user meta; };
+              modules = [
+                inputs.nixos-wsl.nixosModules.default
+                unstable-overlay
+                ./hosts/wsl/configuration.nix
+                # Import the relevant common config
+                # TODO: Update config so this can be ./system
+                ./system/applications
+                ./system/desktop
+                ./system/localization.nix
+                ./system/nixos.nix
+                ./system/system-packages.nix
               ];
             };
       };
---
+++ hosts/wsl/configuration.nix
@@ -0,0 +1,65 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, pkgs, inputs, user, meta, ... }:
+
+{
+  imports = [
+    inputs.home-manager.nixosModules.default
+  ];
+
+  wsl = {
+    enable = true;
+    defaultUser = user.username;
+
+    wslConf = {
+      automount.options = "metadata";
+    };
+  };
+
+  # Do not enable any desktop related settings
+  cfg.desktop.enable = false;
+
+  # User config
+  users.users.${user.username} = {
+    # NOTE: Other config is also managed by NixOS-WSL
+    shell = pkgs.zsh;
+  };
+  # Enable zsh as it is the default shell
+  programs.zsh.enable = true;
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    sharedModules = [
+      inputs.plasma-manager.homeManagerModules.plasma-manager
+    ];
+
+    users = {
+      ${user.username} = import ./home.nix;
+    };
+    extraSpecialArgs = {
+      inherit inputs user meta;
+      # Share custom system config
+      sysCfg = config.cfg;
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable nix-ld for vscode server and csharp development
+  programs.nix-ld.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
