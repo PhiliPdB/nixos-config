5315b55c82e0910dfd390433e1ff4cf9c317b599
PhiliPdB <phlpdbrn@gmail.com>
2025-09-06 10:01:33 +0200

    Add nix-community cache

--- system/nixos.nix
+++ system/nixos.nix
@@ -15,12 +15,10 @@
     trusted-users = [ "root" user.username ];
 
     substituters = [
-      "https://devenv.cachix.org"
-      "https://nixpkgs-python.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
