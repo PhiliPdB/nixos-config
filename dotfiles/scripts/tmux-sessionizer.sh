07b474dc274835b8689f6177aee5b59198fec6ee
PhiliPdB <phlpdbrn@gmail.com>
2025-10-06 20:16:47 +0200

    Stealing my own tmux sessionizer

---
+++ dotfiles/scripts/tmux-sessionizer.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+# Requirements: tmux, fzf
+# Modified from: https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+
+DIRS=(
+    "$HOME"
+    "$HOME/repositories"
+)
+
+# Get selected either from the argument or fzf
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(\
+        find "${DIRS[@]}" -mindepth 1 -maxdepth 1 -type d ! -name ".*" \
+        | fzf \
+    )
+fi
+
+# Check if selected is not empty
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+# If not in tmux, create and attach to new session
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+# Else create detached session (if it does not exist)
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+# Switch to the selected session
+tmux switch-client -t $selected_name
+
--- user/terminal/tmux.nix
+++ user/terminal/tmux.nix
@@ -1,10 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+let
+  dotfilesDir = inputs.self.outputs.dotfiles.default;
+in
 {
   stylix.targets.tmux.enable = false;
 
   # Install tmux sessionizer
   home.packages = with pkgs; [
-    tmux-sessionizer
+    (writeScriptBin "tms" (builtins.readFile (dotfilesDir + /scripts/tmux-sessionizer.sh)))
   ];
 
   # Tmux setup
@@ -12,7 +15,6 @@
     enable = true;
     terminal = "screen-256color";
     clock24 = true;
-    newSession = true;
 
     keyMode = "vi";
     mouse = true;
@@ -25,6 +27,10 @@
       # Reload Tmux config
       unbind r
       bind r source-file ~/.config/tmux/tmux.conf
+
+      ### General behaviour
+
+      set -g detach-on-destroy off
 
       ### Status bar config
 
@@ -40,22 +46,17 @@
 
       # Open new project
       bind o display-popup -w 80% -h 60% -E "tms"
-      bind s display-popup -w 80% -h 60% -E "tms switch"
-      bind C-r "run-shell 'tms refresh'"
 
       # Quick shortcuts
-      bind C-1 "run-shell 'tms marks open 0'"
-      bind C-2 "run-shell 'tms marks open 1'"
-      bind C-3 "run-shell 'tms marks open 2'"
-      bind C-4 "run-shell 'tms marks open 3'"
-
+      bind C-1 "run-shell 'tms ~/nixos-config"
+      # TODO: Add more shortcuts?!
 
       ### Window selection and resizing bindings
 
-      bind -r h select-pane -L
-      bind -r j select-pane -D
-      bind -r k select-pane -U
-      bind -r l select-pane -R
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
 
       bind -r H resize-pane -L 5
       bind -r J resize-pane -D 5
