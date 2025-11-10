639b5127352a771536829eaef1a6adc8e1f83e6d
PhiliPdB <phlpdbrn@gmail.com>
2025-11-10 18:09:44 +0100

    Add cuda cache

--- system/nixos.nix
+++ system/nixos.nix
@@ -10,15 +10,23 @@
   nix.settings = {
     auto-optimise-store = true;
     # Enable flakes
-    experimental-features = [ "nix-command" "flakes" ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
-    trusted-users = [ "root" user.username ];
+    trusted-users = [
+      "root"
+      user.username
+    ];
 
     substituters = [
       "https://nix-community.cachix.org"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 
