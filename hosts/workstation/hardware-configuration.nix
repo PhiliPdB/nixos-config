7a7fb2f2cef682774f6ce563da8479effa15c152
PhiliPdB <phlpdbrn@gmail.com>
2025-11-22 18:52:59 +0100

    PC hardware upgrade

--- hosts/workstation/hardware-configuration.nix
+++ hosts/workstation/hardware-configuration.nix
@@ -15,21 +15,21 @@
   ];
 
   boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
     "nvme"
-    "usb_storage"
+    "ahci"
+    "xhci_pci"
     "usbhid"
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/a944b581-5db8-43f7-aa9c-6de6d36bf31d";
     fsType = "ext4";
   };
+
   fileSystems."/home" = {
     device = "/dev/disk/by-uuid/7fe569e3-4c2a-438c-afc8-b998444ee809";
     fsType = "ext4";
@@ -53,8 +53,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp7s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
