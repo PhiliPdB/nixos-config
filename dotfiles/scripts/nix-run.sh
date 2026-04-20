bc5505b0a101a368c3c6accf215db924c68f6f34
PhiliPdB <phlpdbrn@gmail.com>
2026-04-20 19:33:19 +0200

    Update nix-run script to also run from unstable

--- dotfiles/scripts/nix-run.sh
+++ dotfiles/scripts/nix-run.sh
@@ -1,18 +1,30 @@
 #! /usr/bin/env bash
 
+NIXPKGS_UNSTABLE_URL="github:NixOS/nixpkgs/nixos-unstable"
+
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <package> [-- <args>]"
+    echo "Usage: $0 [unstable/]<package> [-- <args>]"
     exit 1
 fi
 
+# Extract package name
 package="$1"
+
+# Check if packages should be fetched from unstable or stable
+if [[ "$package" == unstable/* ]]; then
+    package="${package#unstable/}"
+    package_ref="$NIXPKGS_UNSTABLE_URL#$package"
+else
+    package_ref="nixpkgs#$package"
+fi
+
 shift
 
 if [ "$1" = "--" ]; then
     shift
     # Pass in optional arguments
-    exec nix run "nixpkgs#$package" -- "$@"
+    exec nix run "$package_ref" -- "$@"
 else
-    exec nix run "nixpkgs#$package"
+    exec nix run "$package_ref"
 fi
 
